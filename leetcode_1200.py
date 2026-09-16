1ce72f8937763bd946c7632b2d316add07178264
daniel <danpauljs@gmail.com>
2026-09-16 10:51:45 +0530

    Added leetcode_1800

---
+++ leetcode_1200.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        result = nums[0]
+        current = nums[0]
+
+        for i in range(1,len(nums)):
+            if nums[i]>nums[i-1]:
+                current+=nums[i]
+            else:
+                current = nums[i]
+            result = max(result,current)
+        return result
+        
