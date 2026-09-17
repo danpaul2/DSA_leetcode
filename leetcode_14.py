0b04d0afce8eea8c2f303005f013db15fc4166a2
daniel <danpauljs@gmail.com>
2026-09-17 21:14:43 +0530

    Added leetcode_14

---
+++ leetcode_14.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        if not strs:
+            return ""
+
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+
+            for string in strs:
+                if i==len(string) or string[i] != char:
+                    return strs[0][:i]
+        
+        return strs[0]
+
+        
+        
